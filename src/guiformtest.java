df0db66f2eb0aafb4db878e17be3fc675965521c
rasmus.gillberg <xxyaymanyayxx@hotmail.com>
2020-03-20 10:05:16 +0100

    notepad fortsatt, ingen funktion funkar ännu men borde gå att fixa

--- src/guiformtest.java
+++ src/guiformtest.java
@@ -1,35 +1,36 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class guiformtest {
 
     private JPanel RootPanel;
     private JTextArea textArea;
-    private JButton ctrlCctrlVButton;
-    private JButton saveAsButton;
-
-
-    public guiformtest() {
-        ctrlCctrlVButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                String text = textArea.getText();
-                textArea.append(text);
-            }
-        });
-    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Nötpadda");
         frame.setContentPane(new guiformtest().RootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
         frame.setSize(300,300);
+
+
+        JMenuBar MenuBar = new JMenuBar();
+        frame.setJMenuBar(MenuBar);
+
+        JMenu file = new JMenu ("File");
+        MenuBar.add(file);
+
+        JMenuItem open = new JMenuItem("Open File");
+            file.add(open);
+
+            JMenuItem save = new JMenuItem("Save As");
+            file.add(save);
+
+        frame.pack();
         frame.setVisible(true);
     }
-
-
-
+    String txt = textArea.getText();
 }
